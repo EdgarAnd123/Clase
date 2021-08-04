5dc8d54252956c8f1e4e45df6a37380e943fb7ba
edgar <sancheze82@uabc.edu.mx>
2021-08-04 11:38:38 -0700

    Activated auth routing

--- src/routing/routes.js
+++ src/routing/routes.js
@@ -9,16 +9,16 @@ export default [
         name: 'login', path: "/", component: login, meta: { hideLayoutComponents: true }
     },
     {
-        name: 'employees', path: "/employees", component: employees// ,meta: { requiresAuth: true }
+        name: 'employees', path: "/employees", component: employees ,meta: { requiresAuth: true }
     },
     {
-        name: 'create-employee', path: "/employees/create", component: createEmployee//, meta: { requiresAuth: true }
+        name: 'create-employee', path: "/employees/create", component: createEmployee, meta: { requiresAuth: true }
     },
     {
-        name: 'profile-employee', path: "/employees/:id", component: profileEmployee//, meta: { requiresAuth: true }
+        name: 'profile-employee', path: "/employees/:id", component: profileEmployee, meta: { requiresAuth: true }
     },
     {
-        name: 'edit-employee', path: "/employees/edit/:id", component: editEmployee//, meta: { requiresAuth: true }
+        name: 'edit-employee', path: "/employees/edit/:id", component: editEmployee, meta: { requiresAuth: true }
     },
     { 
         path: '/*', redirect: '/'
